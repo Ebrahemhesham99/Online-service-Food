ad61eec218589314e7790f0c141fa8dcac901c00
Ebrahem hesham <Ebrahem hesham@DESKTOP-NT1BO0C>
2020-05-11 22:44:22 +0200

    Design the home page(1)

---
+++ src/restaurant/test/RESTAURANTTEST.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant.test;
+import java.util.Scanner;
+import java.awt.*;
+/**
+ *
+ * @author Ebrahem hesham
+ */
+public class RESTAURANTTEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        restaurantFrame rest_frame = new restaurantFrame();
+        rest_frame.createComponent();
+        
+    }
+    
+}
