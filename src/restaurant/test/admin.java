1bebb2752fcc07d45f580218d3d7e87584ac42ab
HadyAtef <62363366+HadyAtef@users.noreply.github.com>
2020-05-28 17:49:51 +0200

    Update admin.java
    Function for updating the items of the menu

--- src/restaurant/test/admin.java
+++ src/restaurant/test/admin.java
@@ -1,1 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw2_project;
 
+public class admin {
+    
+      private DBmanager dbManager = DBmanager.createInstance();
+    
+      
+      public boolean update_menu(String name,String price,String  id){
+    
+    
+      return dbManager.update_item(name, price, id);
+    
+    
+    }
+      
+      public boolean add_item(String name , String price){
+      
+      
+     return dbManager.add_item(name, price);
+      
+      
+      
+      }
+      
+      public boolean delete_item(String name){
+      
+      
+      return dbManager.del_item(name);
+      
+      }
+    
+    
+}
