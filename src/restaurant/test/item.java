c9fbcdcd8a80d856989a6df81544eb035580f221
HadyAtef <62363366+HadyAtef@users.noreply.github.com>
2020-05-31 23:54:38 +0200

    Update item.java
    Creating class for the items

--- src/restaurant/test/item.java
+++ src/restaurant/test/item.java
@@ -1,1 +1,37 @@
+public class Item {
+    private int id;
+    private String name;
+    private String price;
 
+    public Item(){};
+
+    public Item(int id, String name, String price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
